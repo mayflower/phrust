0146f391b5d4cd420d37ad2447eff10b918fa9dc
Johann-Peter Hartmann <johann-peter.hartmann@mayflower.de>
2026-06-20 15:14:21 +0200

    chore(build): speed up Rust dev loop

--- flake.nix
+++ flake.nix
@@ -22,6 +22,10 @@
         let
           pkgs = import nixpkgs { inherit system; };
           inherit (pkgs) lib stdenv;
+          rustcSccacheWrapper = pkgs.writeShellScriptBin "rustc-sccache-wrapper" ''
+            unset CARGO_INCREMENTAL
+            exec ${pkgs.sccache}/bin/sccache "$@"
+          '';
 
           commonPackages = with pkgs; [
             rustc
@@ -48,6 +52,7 @@
             gnumake
             pkg-config
             ccache
+            sccache
 
             cmake
             ninja
@@ -66,6 +71,7 @@
 
           linuxPackages = with pkgs; [
             gdb
+            mold
             valgrind
           ];
 
@@ -85,9 +91,29 @@
             PHP_REF_TAG = "php-8.5.7";
             PHP_REF_REPO = "https://github.com/php/php-src.git";
             RUST_BACKTRACE = "1";
+            RUSTC_WRAPPER = "${rustcSccacheWrapper}/bin/rustc-sccache-wrapper";
+            SCCACHE_CACHE_SIZE = "20G";
+            CARGO_REGISTRIES_CRATES_IO_PROTOCOL = "sparse";
 
-            shellHook = ''
+            shellHook =
+              ''
+                export CARGO_TARGET_DIR="$PWD/target"
+                export SCCACHE_DIR="$PWD/.cache/sccache"
+                export CCACHE_DIR="$PWD/.cache/ccache"
+                mkdir -p "$CARGO_TARGET_DIR" "$SCCACHE_DIR" "$CCACHE_DIR"
+              ''
+              + lib.optionalString stdenv.isLinux ''
+                export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=clang
+                export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=clang
+                case " ${RUSTFLAGS:-} " in
+                  *" -C link-arg=-fuse-ld=mold "*) ;;
+                  *) export RUSTFLAGS="-C link-arg=-fuse-ld=mold ${RUSTFLAGS:-}" ;;
+                esac
+              ''
+              + ''
               echo "phrust dev shell" >&2
+              echo "  Rust cache: $SCCACHE_DIR" >&2
+              echo "  Cargo target: $CARGO_TARGET_DIR" >&2
               echo "  just help" >&2
               echo "  just verify" >&2
             '';
